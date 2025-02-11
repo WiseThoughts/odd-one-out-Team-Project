942bbde25a2e44ffb8e9796cb16e8b1431de99c6
WiseTheoughts <benny.nebreda@gmail.com>
2025-02-11 03:48:17 +0000

    Set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          The start of the ODD ONE OUT Team Project.
+        </p>
+    </div>
+  );
+}
+
+export default App;
