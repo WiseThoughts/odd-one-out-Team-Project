8eb6bcba83149f6005f112f9c0cf3fbc8e04ce69
Maria <Maria.Pan7@outlook.com>
2025-03-11 15:24:13 +0000

    Designed the main webpage layout

--- src/components/Introduction/introduction.js
+++ src/components/Introduction/introduction.js
@@ -4,7 +4,19 @@ import './introduction.css';
 const introduction = () => {
 
   return (
-    <div>introduction</div>
+    <div class="intro">
+      <h2><u>How To Play</u></h2>
+      <br></br>
+      <h3>Setting up the game:</h3>
+      <p>One person will generate the link for the game room and share it with the rest of the group.<br></br> 
+      The rest of the players will input the link, joining the game.</p><br></br>
+      <h3>Game Instructions:</h3>
+      <p>One player will get 'TRAITOR' on their screen and the rest will get a word shared between them.<br></br>
+      All players will need to give vague hints towards what the word is, trying to figure out who the traitor is.<br></br>
+      The goal of the traitor is to blend in as a player and figure out the word being hinted.<br></br>
+      Once a player has figured out who the traitor is, they can select the player from the player list given.<br></br>
+      On the other hand, if the traitor figures out the word, they can select it from the word list give to them.</p>
+    </div>
   )
 }
 
--- src/pages/App.js
+++ src/pages/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Background from '../components/Background/background';
 import Title from '../components/Title/title';
+import Introduction from '../components/Introduction/introduction';
 
 
 function App() {
@@ -9,7 +10,11 @@ function App() {
     <div>
       <Title />
       <Background />
-        
+      <div class="flexContainer">
+      <div class="introBox"><Introduction /></div>
+      <div><button class="gameLinkButtons">Generate game link</button>
+      <button class="gameLinkButtons">Input game link</button></div>
+      </div>
     </div>
   );
 }
