4788b10dd1236be2cd163dc03ac385641a7da514
valentinacakalli <23733303@stu.mmu.ac.uk>
2025-03-20 10:04:23 +0000

    Update room-creation.js

--- src/pages/Room Creation Link/room-creation.js
+++ src/pages/Room Creation Link/room-creation.js
@@ -6,11 +6,22 @@ import Title from '../../components/Title/title';
 
 const roomCreation = () => {
   return (
-    <div>
-        <Title />
-        <Background />
+    <div>className="room-creation-container" 
+    <Title />
+    <Background />
+    
+    <div className="container">
+      <p>The game link is</p>
+      <div className="game-link">-------------------------------</div>
+      <p>Please share it with the players</p>
+      <p><span class="players-count">0</span> Players joined</p>
+      <button className="start-btn">Start game</button>
     </div>
-  )
+      
+    </div>
+
+  
+  );
 }
 
-export default roomCreation
+export default roomCreation 
